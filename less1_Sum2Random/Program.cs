dcc46acb52a114187ffae6b62a3765219cba919a
Alina Malyutina <linamalutina1@gmail.com>
2022-10-28 23:17:18 +0300

    add random sum

---
+++ less1_Sum2Random/Program.cs
@@ -0,0 +1,7 @@
+﻿int number1 = new Random().Next(1, 10);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
+int sum = number1 + number2;
+Console.Write("SUM = ");
+Console.WriteLine(sum);
