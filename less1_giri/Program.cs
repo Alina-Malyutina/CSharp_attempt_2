f3e5165f98e1b2bf68f769336f2c4892ed3bc0f3
Alina Malyutina <linamalutina1@gmail.com>
2022-10-28 23:56:53 +0300

    add array with kettlebells

---
+++ less1_giri/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] kettlebells = {1, 8, 3, 2, 6};
+int max = 0;
+for(int i = 0; i < kettlebells.Length; i++)
+{
+    if(kettlebells[i] > max)
+    {
+        max = kettlebells[i];
+    }
+}
+Console.Write("max = ");
+Console.Write(max);
