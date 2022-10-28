f1c5998b018a89c33c0a1fa3a77752c54eb32e29
Alina Malyutina <linamalutina1@gmail.com>
2022-10-28 23:34:38 +0300

    Add welcome to my darling

---
+++ less1_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your username");
+string username = Console.ReadLine();
+if(username == "Yasumi")
+{
+    Console.WriteLine("Hi, my darling <3");
+}
+else
+{
+    Console.WriteLine("Fuck you, ");
+    Console.WriteLine(username);
+}
